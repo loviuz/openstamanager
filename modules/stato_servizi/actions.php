b95fad1d588720dd354fe8b716614f13cd8dcc8b
Dasc3er <Dasc3er@users.noreply.github.com>
2020-12-31 16:13:28 +0100

    Introduzione modifiche per compatibilità con Laravel

--- modules/stato_servizi/actions.php
+++ modules/stato_servizi/actions.php
@@ -141,7 +141,7 @@ switch (filter('op')) {
     case 'sizes':
         $results = [];
 
-        $backup_dir = App::getConfig()['backup_dir'];
+        $backup_dir = AppLegacy::getConfig()['backup_dir'];
 
         $dirs = [
             $backup_dir => tr('Backup'),
