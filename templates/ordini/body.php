708d1a6177b941f15b8b54784f38703f0966ab69
Dasc3er <Dasc3er@users.noreply.github.com>
2021-01-04 18:54:23 +0100

    Rimozione Translator

--- templates/ordini/body.php
+++ templates/ordini/body.php
@@ -163,7 +163,7 @@ foreach ($righe as $riga) {
 
         echo '
             <td class="text-center">
-                '.Translator::numberToLocale(abs($qta), 'qta').' '.$um.'
+                '.numberFormat(abs($qta), 'qta').' '.$um.'
             </td>';
 
         if ($options['pricing']) {
@@ -193,7 +193,7 @@ foreach ($righe as $riga) {
             // Iva
             echo '
             <td class="text-center">
-                '.Translator::numberToLocale($riga->aliquota->percentuale, 0).'
+                '.numberFormat($riga->aliquota->percentuale, 0).'
             </td>';
         }
 
